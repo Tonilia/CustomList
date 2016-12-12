e4fa1c43ab2b37254f77b12335ac923a14033012
Tonilia <skatinganimal@yahoo.com>
2016-12-12 10:39:56 -0600

    just started

---
+++ customList.cs/GenericClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customList.cs
+{
+    class GenericClass
+    {
+        public void GetAnimalName<T>()
+        {
+            List<string> animalName = new List<string>();
+        }
+    }
+}
